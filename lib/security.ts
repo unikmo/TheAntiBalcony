78f4c2f87bae81ed0e9ec7f2e627d6ebe8d9eaa7
unikmo <mbanwie@googlemail.com>
2026-08-22 22:26:48 +0200

    Add tested escaping helpers for public Ring artifacts

---
+++ lib/security.ts
@@ -0,0 +1,16 @@
+export function safeJsonLd(value: unknown) {
+  return JSON.stringify(value)
+    .replace(/</g, "\\u003c")
+    .replace(/>/g, "\\u003e")
+    .replace(/&/g, "\\u0026");
+}
+
+export function escapeHtmlAttribute(value: string) {
+  return value.replace(/[&<>"']/g, (char) => ({
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#039;",
+  })[char] || char);
+}
